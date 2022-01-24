d9e8125b5ce272532f1e0ef45c833eb8357eda62
HuMiao <humiao2006@gmail.com>
2022-01-24 15:57:06 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// Datafeed implementation, will be added later
+import Datafeed from './datafeed.js';
+
+//创建组件
+window.tvWidget = new TradingView.widget({
+    symbol: 'Bitfinex:BTC/USD', // default symbol
+    interval: '1D', // default interval
+    fullscreen: true, // displays the chart in the fullscreen mode
+    container: 'tv_chart_container',
+    datafeed: Datafeed,
+    library_path: '../charting_library_clonned_data/charting_library/',
+    //关闭功能， 去掉图标 :widget_logo
+    disabled_features: ["widget_logo", "left_toolbar"],
+});
